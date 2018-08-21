679315d7ecc5683815be51c7352aa655ebbdfc69
Van Le <ducvan.le@ihg.com>
2018-08-21 13:14:14 -0400

    pipe example

---
+++ src/app/shared/pipes/my-upper-case.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myUpperCase'
+})
+export class MyUpperCasePipe implements PipeTransform {
+
+  transform(value: any): any {
+    return String(value).toUpperCase();
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { UnorderedListComponent } from './components/unordered-list/unordered-list.component';
 import { ToolHeaderComponent } from './components/tool-header/tool-header.component';
+import { MyUpperCasePipe } from './pipes/my-upper-case.pipe';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UnorderedListComponent, ToolHeaderComponent],
-  exports: [UnorderedListComponent, ToolHeaderComponent],
+  declarations: [UnorderedListComponent, ToolHeaderComponent, MyUpperCasePipe],
+  exports: [UnorderedListComponent, ToolHeaderComponent, MyUpperCasePipe],
 })
 export class SharedModule { }
